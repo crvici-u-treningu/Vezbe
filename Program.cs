833fa97a084667a777ba859ecc1df52cc1f2bbb6
Miroslav <gavrilov.miroslav@gmail.com>
2020-03-10 20:23:39 +0100

    Novi projekat podignut.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Vezbe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
