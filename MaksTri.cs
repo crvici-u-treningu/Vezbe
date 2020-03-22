98c0a22ba048aa13c9bbec583bcdde68b8b5888c
Crvici <gataricmilena@gmail.com>
2020-03-22 13:13:08 +0100

    Update MaksTri.cs

--- MaksTri.cs
+++ MaksTri.cs
@@ -8,6 +8,22 @@ namespace Vezbe
     {
         public static int DajMaksTri(int a, int b, int c)
         {
+            if (a > b && a > c)
+            { 
+                Console.WriteLine("a");
+            }
+            
+            {
+                if (b > c && b > a)
+                {
+                    Console.WriteLine("b");
+                }
+            
+                if (c > a && c > b)
+                {
+                    Console.WriteLine("c");
+                }
+            }
             return 0;
         }
     }
