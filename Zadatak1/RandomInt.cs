0df84593085876ddfeee23b7cc58287ea9c4bcec
Miroslav <gavrilov.miroslav@gmail.com>
2020-04-05 17:28:28 +0200

    Create RandomInt.cs

---
+++ Zadatak1/RandomInt.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zadatak1
+{
+    public static class RandomInt
+    {
+        private static readonly Random r = new Random();
+
+        public static int GetNew(int min, int max)
+        {
+            return r.Next(min, max + 1);
+        }
+    }
+}
